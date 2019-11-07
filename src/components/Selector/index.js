a66da36274bf22ad11e42f0bcf026776a4efb67a
Igor Vachinskiy <i.vachinskiy@adaptivestone.com>
2019-11-07 20:18:06 +0300

    fix

--- src/components/Selector/index.js
+++ src/components/Selector/index.js
@@ -1,31 +1,9 @@
 import React, { useState } from 'react';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { FormControl, Select, MenuItem } from '@material-ui/core';
 import i18n from 'i18next';
 import Language from '../../helpers/language';
 
-
-const useStyles = makeStyles((theme) =>
-    createStyles({
-        root: {
-            position: 'fixed',
-            bottom: theme.spacing(2),
-            right: theme.spacing(2),
-        },
-        paper: {
-            padding: theme.spacing(2),
-            textAlign: 'center',
-            color: theme.palette.text.secondary,
-            'margin-top': 20,
-            'margin-left': 20,
-            width: 150,
-            height: 150,
-        },
-    }),
-);
-
 const Selector = () => {
-    const classes = useStyles();
     const [local, setLocal] = useState(Language());
     const handleChange = (event) => {
         setLocal(event.target.value);
@@ -34,7 +12,7 @@ const Selector = () => {
     };
 
     return (
-        <FormControl variant="outlined" className={classes.formControl}>
+        <FormControl variant="outlined" >
             <Select
                 labelId="demo-simple-select-outlined-label"
                 id="demo-simple-select-outlined"
--- src/page/Header/index.js
+++ src/page/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import cls from './style.module.scss'
 import { useSelector } from 'react-redux'
 import Input from '../../components/Input';
@@ -8,6 +8,8 @@ import { withRouter } from "react-router";
 import SelectorCode from '../../components/SelectorCode';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
+import defaultImg from '../../img/51_full.jpg'
+import logoImg from '../../img/countries.png'
 
 const Header = ({ location }) => {
     const { t } = useTranslation()
@@ -15,15 +17,21 @@ const Header = ({ location }) => {
         ignoreQueryPrefix: true,
     })
     const photoHeader = useSelector((state) => state.countries.photoHeader)
+    const [src,setSrc] = useState(photoHeader)
+    useEffect(()=>{
+        setSrc(photoHeader)
+    },[photoHeader])
     return (
         <div className={cls.header}>
-            <img className={cls.header_img} src={photoHeader} alt='header logo' />
+            <img className={cls.header_img} src={src} onError={(e,a)=>{
+                setSrc(defaultImg)
+                }} alt='header logo' />
             <div className={cls.header_conteiner} id="back-to-top-anchor">
                 <div className={cls.header_content}>
                     <div className={cls.container_logo_title}>
                         <div className={cls.header_content_log}>
                             <div className={cls.logo_content}>
-                                <img src={'https://ru.seaicons.com/wp-content/uploads/2015/06/Maps-Globe-Filled-icon.png'} alt='logo' />
+                                <img src={logoImg} alt='logo' />
                             </div>
                         </div>
                         <div className={cls.header_content_menu}>
